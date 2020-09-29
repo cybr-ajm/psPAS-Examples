c83e4d6d7ec6848997882ebc03286b45b18394ee
Adam Markert <56974554+cybr-ajm@users.noreply.github.com>
2020-09-29 09:29:54 -0400

    Update rootimport.ps1

--- rootimport.ps1
+++ rootimport.ps1
@@ -14,17 +14,17 @@ Import-Module psPAS
 ###DEFINE VARIABLES###
 
 #Import CSV File
-$CSVPath = "C:\Users\john\Downloads\psPAS-Examples-master\psPAS-Examples-master\rootimport.csv"
+$CSVPath = "C:\rootimport.csv"
 $CSVFile = import-csv -Path $CSVPath
 
 #define a Cyberark User with access to add accounts to the target safe, Administrator in this case
 $apiCred = Get-Credential Administrator
 
 #PVWA Address without /PasswordVault
-$PVWA_Address = "https://comp01.cybr.com"
+$PVWA_Address = "https://myPVWA"
 
 #LDAP Integration Domain
-$searchDomain = 'cybr.com'
+$searchDomain = 'myDomain.com'
 
 
 #####BEGIN PROVISIONING#####
